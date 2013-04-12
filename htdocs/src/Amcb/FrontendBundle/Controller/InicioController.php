cf988c3a9bc94c801e03f4cb6cd841b3e68e910a
Dani Sancas <lord.sancas@gmail.com>
2013-04-12 17:06:10 +0200

    - Comienzo a crear las páginas estáticas de Amcb/FrontendBundle
    TODO
    - Crear el modelo al completo (basarse en lo ya hecho en el proyecto empezado en Symfony 1.4)
    - Crear el resto de páginas estáticas de Amcb/FrontendBundle
    - Crear Amcb/BackendBundle, a ser posible con un generador de administración
    
    Signed-off-by: Dani Sancas <lord.sancas@gmail.com>

---
+++ htdocs/src/Amcb/FrontendBundle/Controller/InicioController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Amcb\FrontendBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class InicioController extends Controller
+{
+    /**
+     * 
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+    
+    /**
+     * 
+     * @Template()
+     */
+    public function historiaAction()
+    {
+        return array();
+    }
+    
+    /**
+     * 
+     * @Template()
+     */
+    public function escuchanosAction()
+    {
+        return array();
+    }
+    
+    /**
+     * 
+     * @Template()
+     */
+    public function repertorioAction()
+    {
+        return array();
+    }
+    
+    /**
+     * 
+     * @Template()
+     */
+    public function docsInteresAction()
+    {
+        return array();
+    }
+}
