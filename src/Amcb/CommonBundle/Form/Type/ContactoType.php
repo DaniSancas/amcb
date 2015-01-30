aaf5368578e9fad578feb1932048a57b60c3aa49
Dani Sancas <lord.sancas@gmail.com>
2015-01-30 14:33:12 +0100

    Minor fixes
    TODO
    - Diseño responsive CSS3
    - Páginas de error personalizadas por cada Bundle
    - Tests unitarios y funcionales
    
    Signed-off-by: Dani Sancas <lord.sancas@gmail.com>

--- src/Amcb/CommonBundle/Form/Type/ContactoType.php
+++ src/Amcb/CommonBundle/Form/Type/ContactoType.php
@@ -4,7 +4,6 @@ namespace Amcb\CommonBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\DependencyInjection\Container as Container;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
